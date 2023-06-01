20e487e677ab8cb0983e9de2fe266992e14c5473
Unsigned Void <gabriel-garciac@hotmail.com>
2023-05-31 23:41:40 -0300

    improved versions of second and third stage

--- EncryptMathPOG/main.py
+++ EncryptMathPOG/main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import math
 import PySimpleGUI as sg
 
 path = os.getcwd()
@@ -22,38 +23,94 @@ for x in randomNums:
 def encrypt(word, stage):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-    if stage == 0:
+    if stage == 0: #Caesar Stage
 
-        key1 = random.randint(65,89) 
-        offset = key1 - 64
-        word1 = ''.join(( chr( 97 + ( ord(l) - 97 + offset ) % 26) for l in word))
-        cAlphabet = ''.join(( chr( 97 + ( ord(letter) - 97 + offset ) % 26) for letter in alphabet))
-        key = chr(key1) 
+        difficulty = random.randint(0,3)
 
-    elif stage == 1:
+        if difficulty == 0:
+            key1 = random.randint(65,89) 
+            offset = key1 - 64
+            word1 = ''.join(( chr( 97 + ( ord(l) - 97 + offset ) % 26) for l in word))
+            cAlphabet = ''.join(( chr( 97 + ( ord(letter) - 97 + offset ) % 26) for letter in alphabet))
+            key = chr(key1) 
 
-        key1 = random.randint(97,121) 
-        offset = (key1 - 96) * -1
-        word1 = ''.join(( chr( 97 + ( ord(l) - 97 + offset ) % 26) for l in word))
-        cAlphabet = ''.join(( chr( 97 + ( ord(letter) - 97 + offset ) % 26) for letter in alphabet))
-        key = chr(key1) 
+        elif difficulty == 1:
+            key1 = random.randint(97,121) 
+            offset = (key1 - 96) * -1
+            word1 = ''.join(( chr( 97 + ( ord(l) - 97 + offset ) % 26) for l in word))
+            cAlphabet = ''.join(( chr( 97 + ( ord(letter) - 97 + offset ) % 26) for letter in alphabet))
+            key = chr(key1) 
 
-    elif stage == 2:
+        else:
+            key1 = random.randint(65,90) 
+            key2 = random.randint(97,122)
+
+            if key1 == key2:
+                while key1 != key2:
+                    key2 = random.randint(97,122)
+
+            offset = (key1 - 64) + ((key2 - 96) * -1)
+
+            word1 = ''.join(( chr( 97 + ( ord(l) - 97 + offset ) % 26) for l in word))
+
+            cAlphabet = ''.join(( chr( 97 + ( ord(letter) - 97 + offset ) % 26) for letter in alphabet))
+            
+            key = chr(key1) + chr(key2)
+
+        for x in range(0, len(alphabet)):
+            if(alphabet[x] in word):
+                alphabet = alphabet[:x] + alphabet[x].upper() + alphabet[(x+1):]
+                cAlphabet = cAlphabet[:x] + cAlphabet[x].upper() + cAlphabet[(x+1):]
+
+        alphabet = alphabet.replace("", " ")[1: -1]
+        cAlphabet = cAlphabet.replace("", " ")[1: -1]
+
+        return [word1,key,alphabet, cAlphabet]
+
+
+    elif stage == 1: #Circle Stage
 
         key1 = random.randint(65,90) 
-        key2 = random.randint(97,122)
+        while math.floor(((key1 - 64) * (key1 - 64) * 3.14) % 26) == 26:
+            key1 = random.randint(65,90) 
 
-        if key1 == key2:
-            while key1 != key2:
-                key2 = random.randint(97,122)
-
-        offset = (key1 - 64) + ((key2 - 96) * -1)
+        offset = (key1 - 64) * (key1 - 64) * 3.14
+        offset = math.floor(offset)
 
         word1 = ''.join(( chr( 97 + ( ord(l) - 97 + offset ) % 26) for l in word))
 
         cAlphabet = ''.join(( chr( 97 + ( ord(letter) - 97 + offset ) % 26) for letter in alphabet))
+
+        key = chr(key1) 
+
+    elif stage == 2: # TimeCube Stage
+
+        key1 = random.randint(128336,128346) #- 128335
+        key2 = random.randint(128348,128359) #- 128347 + 0.5
+
+        time = key1 if bool(random.randint(0, 1)) else key2
+
+        offset = time - 128335 if time < 128348 else time - 128347 + 0.5
+
+        offset = offset * 60
+        offset = offset * offset * offset
+
+        while offset % 26 == 0 or offset == 26:
+            key1 = random.randint(128336,128346) #- 128335
+            key2 = random.randint(128348,128359) #- 128347 + 0.5
+
+            time = key1 if bool(random.randint(0, 1)) else key2
+
+            offset = time - 128335 if time < 128348 else time - 128347 + 0.5
+
+            offset = offset * 60
+            offset = offset * offset * offset
+
+        word1 = ''.join(( chr(math.trunc( 97 + ( ord(l) - 97 + offset ) % 26)) for l in word))
+
+        cAlphabet = ''.join(( chr(math.trunc( 97 + ( ord(letter) - 97 + offset ) % 26)) for letter in alphabet))
         
-        key = chr(key1) + chr(key2)
+        key = chr(time)
 
     for x in range(0, len(alphabet)):
         if(alphabet[x] in word):
@@ -78,7 +135,8 @@ layout = [  [sg.Text( text = "Crypto: " + currentCrypto[0] , key = "-Crypto-" , 
             [sg.Button('Enter', k = '-ENTER-', bind_return_key = True), sg.Button('Exit') ],
             [sg.Text( text = currentCrypto[2], key = "-Help0-" , text_color = "white", font = ("Courier",20),visible = hintVisible)],
             [sg.Text( text = currentCrypto[3], key = "-Help1-" , text_color = "white", font = ("Courier",20),visible = hintVisible )],
-            [sg.Text( text = questions[currentStage], key = "-Help2-" , text_color = "white", font = ("Courier",10),visible = hintVisible )] ]
+            [sg.Text( text = "", key = "-Help2-" , text_color = "white", font = ("Courier",15),visible = hintVisible )],
+            [sg.Text( text = questions[currentStage], key = "-Help3-" , text_color = "white", font = ("Courier",10),visible = hintVisible )] ]
 
 window = sg.Window('EncryptPOG', layout,element_justification='c',finalize=True,return_keyboard_events=True)
 
@@ -95,11 +153,30 @@ while True:
                 currentCrypto = encrypt(questions[currentStage],currentStage)
 
                 window['-Crypto-'].update(value="Crypto: " + currentCrypto[0])
-                window['-Key-'].update(value="Key: " +currentCrypto[1])
+                window['-Key-'].update(value="Key: " + currentCrypto[1])
                 window['-Help0-'].update(value=currentCrypto[2])
                 window['-Help1-'].update(value=currentCrypto[3])
-                window['-Help2-'].update(value=questions[currentStage])
+                if currentStage == 1 :
+                    char = ord(currentCrypto[1]) - 64
+                    result = char * char * 3.14
+                    window['-Help2-'].update("π * {0}² = {1} ☛ {2} % 26 = {3}".format(char,result,result,math.floor(result % 26)))
+                if currentStage == 2 :
+                    key = currentCrypto[1]
+                    keyToTime = ord(key) - 128335 if ord(key) < 128348 else ord(key) - 128347 + 0.5
+                    timeToMin = keyToTime * 60
+                    minSquared = timeToMin*timeToMin*timeToMin
+                    minSquared = math.trunc(minSquared)
+                    squaredMod = minSquared % 26
+                    window['-Help2-'].update(key + "={0} ☛ {1} * 60 = {2} ☛ {3}³(❒) = {4} ☛ {5} % 26 = {6}".format(keyToTime,keyToTime,timeToMin,timeToMin,minSquared,minSquared,squaredMod))
+                
+                window['-Help3-'].update(value=questions[currentStage])
                 window['-INPUT-'].update('')
+
+                if currentStage == 1:
+                   window['-Crypto-'].update(value="Crypto ◯ : " + currentCrypto[0])
+
+                if currentStage == 2:
+                   window['-Crypto-'].update(value="Crypto ⏳ = ❒ : " + currentCrypto[0])
 
             else:
                 sg.Popup("Correct Answer :D",custom_text = "Bye",no_titlebar = True, text_color = "Green",font = ("Arial Black",50))
@@ -113,6 +190,7 @@ while True:
         window['-Help0-'].update(visible=hintVisible)
         window['-Help1-'].update(visible=hintVisible)
         window['-Help2-'].update(visible=hintVisible)
+        window['-Help3-'].update(visible=hintVisible)
         window['-INPUT-'].update('')
         
 
